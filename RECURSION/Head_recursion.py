76400d2e7f7615a2cb94fbab8cfc6639a676a2c8
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-09-06 00:01:58 +0530

    Tail Recusion

--- RECURSION/Head_recursion.py
+++ RECURSION/Head_recursion.py
@@ -7,3 +7,14 @@ def greet(count):
     # then afer the calling all function it will print an "hello"
     print("hello")
 greet(0)            # Fixed: Passed the initial value (0) to start
+
+# Tail Recursion
+def greet(count):
+    if count == 4:
+        return
+    print("hello")
+    # in this case the function works same as normal function but in 
+    # first the printing occurs and then the function jump on the next call likewise 0,1,2,3,4
+    count += 1
+    greet(count)     
+greet(0)           
