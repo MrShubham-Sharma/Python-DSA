c7602774706b4a5cf74eb996b4e8a962c4455dfa
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-09-05 23:23:32 +0530

    Infinite Recursion

---
+++ RECURSION/infinite_recursion.py
@@ -0,0 +1,6 @@
+def Greet():
+    print("Hello")
+    # the function called inside of the loop so it will continue prints the Hello
+    Greet()
+# Loop will start when the function will get call
+Greet()
