2903cc5b5fae1dc63324af2038ef1f34f5341576
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-09-05 23:40:31 +0530

    resovle the infinite loop by declearing Goabal variable and return function

--- RECURSION/infinite_recursion.py
+++ RECURSION/infinite_recursion.py
@@ -1,6 +1,11 @@
+count =0
 def Greet():
+    global count
+    if count==4:
+        return
     print("Hello")
     # the function called inside of the loop so it will continue prints the Hello
+    count+=1
     Greet()
 # Loop will start when the function will get call
 Greet()
