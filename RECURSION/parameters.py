16ea56696250be363f6a8f4dd2a6c688550a10bf
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-09-07 23:56:46 +0530

    1 to N using Recursion

--- RECURSION/parameters.py
+++ RECURSION/parameters.py
@@ -11,3 +11,14 @@ def my_funct(x,n):
     my_funct(x,n-1)
 # we give an values to function
 my_funct("Hello",5)
+
+# print 1 to N using Recursion
+def my_funct(i,n):
+
+    # so here we go with i we need to print till N so we need n must be grater 
+    if i>n:
+        return
+    print(i)
+    # here we need to add i +1 means 1+1=2 till 4
+    my_funct(i+1,n)
+my_funct(1,4)
