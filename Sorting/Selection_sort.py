a2e151f17119bf416fea2f61c598de7c0000a15a
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-09-20 22:39:46 +0530

    Selection sort in Dessending Order

--- Sorting/Selection_sort.py
+++ Sorting/Selection_sort.py
@@ -16,3 +16,16 @@ num_list = [2, 24, 6, 7, 5, 1, 3, 8, 9]
 # Call function and print result
 sorted_list = selection_sort(num_list)
 print(sorted_list)
+
+def decending_sort(num):
+    n=len(num)
+    for i in range(0,n):
+        max_index=i
+        for j in range(i+1,n):
+            if num[j]>num[max_index]:
+                max_index=j
+        num[i],num[max_index]=num[max_index],num[i]
+    return num
+num_list=[1,3,5,8,9,6,7]
+sorted_list=decending_sort(num_list)
+print(sorted_list)
